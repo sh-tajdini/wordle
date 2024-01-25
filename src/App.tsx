52128799880562789a21f8ab28fc991aa4654abd
Shervin Tajdini <shervin.taj@gmail.com>
2024-01-25 18:05:58 -0500

    implementing main page with ability to catch keyboard keys

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,52 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useEffect, useState } from 'react';
+import { Grid } from './components/grid/Grid';
+import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [currentGuess, setCurrentGuess] = useState<string[]>([]);
+  const [guesses, setGuesses] = useState<string[]>([]);
+
+  const handleKeyDown= (event: React.KeyboardEvent) => {
+      if (event.key === 'Enter' && currentGuess.length === 5) {
+      setGuesses((prevGuesses) => [...prevGuesses, currentGuess.join('') as string]);
+      setCurrentGuess([]);
+      return;
+    }
+    // clean the cells by pressing backspace
+      if (event.key === 'Backspace') {
+      setCurrentGuess((prevGuesses) => prevGuesses.slice(0, -1));
+      return;
+    }
+    //only het 5 letter
+      if (currentGuess.length === 5) {
+      return;
+    }
+    // to prevent catching enter and other keyword than letter
+      if (event.key.length > 1) {
+      return;
+    }
+    event.preventDefault();
+    setCurrentGuess((prevGuesses) => [...prevGuesses, event.key]);
+  };
+
+  useEffect(() => {
+    window.addEventListener('keydown', handleKeyDown);
+    return () => window.removeEventListener('keydown', handleKeyDown);
+  }, [currentGuess]);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+<div>
+<div className="header">
+    <h1>Wordle</h1>
+ </div>
+      <div className="board-container">
+        <div className="board">
+          <Grid guesses={guesses} currentGuess={currentGuess} />
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
 
-export default App
+</div>
+  );
+};
+
+export default App;
