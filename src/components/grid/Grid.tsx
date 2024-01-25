5ec4fe73c7cdfc36d9c3432a36ce0d81acfe7c3b
Shervin Tajdini <shervin.taj@gmail.com>
2024-01-25 18:04:35 -0500

    implementing grid page and related style

---
+++ src/components/grid/Grid.tsx
@@ -0,0 +1,11 @@
+import { GuessPropsType } from '../../schema/guess';
+import styles from './Grid.module.css';
+
+export const Grid = ({ guesses, currentGuess }: GuessPropsType) => {
+
+  return (
+    <div className ={styles.boardRow}>
+
+    </div>
+  )
+}
