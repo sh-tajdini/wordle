65f197153abf8e478125c12d4b6ecbbc35099d7f
Shervin Tajdini <shervin.taj@gmail.com>
2024-01-25 11:43:55 -0500

    updating parent reducer based on the slice reducers

--- src/reducers/Game/gameReducer.ts
+++ src/reducers/Game/gameReducer.ts
@@ -3,6 +3,10 @@ import { GameStoreType } from '../../schema/Game';
 
 // reducer slices
 import startGame from './startGame';
+import validateGuess from './validateGuess';
+import endGame from './endGame';
+import updateScore from './updateScore';
+import makeGuess from './makeGuess';
 
 const initialState:GameStoreType = {
   gameStarted: false,
@@ -16,10 +20,19 @@ export const gameSlice = createSlice({
   initialState,
   reducers: {
     ...startGame,
+    ...validateGuess,
+    ...endGame,
+    ...updateScore,
+    ...makeGuess,
+
   },
 });
 const { actions, reducer } = gameSlice;
 export const { doStartGame } = actions;
+export const { doEndGame } = actions;
+export const { doUpdateScore } = actions;
+export const { doValidateGuess } = actions;
+export const { doMakeGuess } = actions;
 
 export default reducer;
 
