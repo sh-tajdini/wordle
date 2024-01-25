72e06b4b69dc023381637ea13252825491f6f518
Shervin Tajdini <shervin.taj@gmail.com>
2024-01-25 18:26:59 -0500

    implementing the structure of filling rows

--- src/components/grid/Grid.tsx
+++ src/components/grid/Grid.tsx
@@ -1,11 +1,21 @@
 import { GuessPropsType } from '../../schema/guess';
+import { CompletedRow } from './CompletedRow'
+import { CurrentRow } from './CurrentRow'
+import { EmptyRow } from './EmptyRow'
 import styles from './Grid.module.css';
 
 export const Grid = ({ guesses, currentGuess }: GuessPropsType) => {
-
+  const empties =
+    guesses.length < 5 ? Array.from(Array(5 - guesses.length)) : []
   return (
     <div className ={styles.boardRow}>
-
+      {guesses.map((guess, i) => (
+        <CompletedRow key={i} guess={guess} />
+      ))}
+      {guesses.length < 6 && <CurrentRow guess={currentGuess} />}
+      {empties.map((_, i) => (
+        <EmptyRow key={i} />
+      ))}
     </div>
   )
 }
